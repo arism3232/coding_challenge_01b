335570b65093be684c0d3cce6d8ea692eeaa1920
Aritree Hoque <aritree.hoque@gmail.com>
2025-01-25 22:07:33 -0500

    Task2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,12 @@ var isActive = true;
 console.log("Employee Name:", employeeName, "-", typeof employeeName);
 console.log("Employee ID:", employeeID, "-", typeof employeeID);
 console.log("Active:", isActive, '-', typeof isActive);
+
+// Task 2 - Primitive Data Types
+let productName = "Microphone";
+const productPrice = 500;
+var isAvailable = true;
+
+console.log("Product Name:", productName, "-", typeof productName);
+console.log("Product Price:", productPrice, "-", typeof productPrice);
+console.log("Available:", isAvailable, "-", typeof isAvailable);
