1c5508da1464880c662e28c8d873e672a0c87429
Aritree Hoque <aritree.hoque@gmail.com>
2025-01-25 22:05:44 -0500

    Task1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 1 - Variables
+let employeeName = "Shin Ryujin";
+const employeeID = 417;
+var isActive = true;
+
+console.log("Employee Name:", employeeName, "-", typeof employeeName);
+console.log("Employee ID:", employeeID, "-", typeof employeeID);
+console.log("Active:", isActive, '-', typeof isActive);
